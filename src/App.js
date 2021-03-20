e128964a926f7c5afd6594092f4a90c69ac7ac63
sitek94 <msitkowski94@gmail.com>
2021-03-20 16:15:28 +0100

    Bring it all together (finished example)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as LogoSvg } from './logo.svg';
+import ThemeToggler from './ThemeToggler';
+import theme from './theme';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Wrapper>
+      <Header>
+        <Logo />
         <h1>Themes with React & CSS Variables</h1>
-      </header>
-    </div>
+        <ThemeToggler />
+      </Header>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+
+  color: ${theme.palette.primary};
+  background-color: ${theme.palette.secondary};
+  transition: ${theme.transitions.default};
+`;
+
+const Logo = styled(LogoSvg)`
+  height: 40vmin;
+  pointer-events: none;
+
+  /* This works because I've manually removed inline styles in logo.svg */
+  fill: ${theme.palette.tertiary};
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${theme.animations.rotate} infinite 20s linear;
+  }
+`;
