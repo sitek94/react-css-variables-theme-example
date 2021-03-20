cbc21434c9429efeeb0632fa3c2bc24d1534b8df
sitek94 <msitkowski94@gmail.com>
2021-03-20 10:00:24 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Themes with React & CSS Variables</h1>
+      </header>
+    </div>
+  );
+}
